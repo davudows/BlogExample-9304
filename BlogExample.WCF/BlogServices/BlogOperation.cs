58bfb081a1779cc69489e4c1d442a16c38660027
İlker ERHALIM <ilkererhalim@gmail.com>
2014-12-22 21:31:00 +0200

    Wcf eklendi

---
+++ BlogExample.WCF/BlogServices/BlogOperation.cs
@@ -0,0 +1,29 @@
+﻿using BlogExample.Services.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogExample.WCF.BlogServices
+{
+    public class BlogOperation:IBlogOperation
+    {
+       
+        public List<Data.Orm.Blog> GetAll()
+        {
+            using (ServicePoint services = new ServicePoint())
+            {
+                return services.Blog.GetAll();
+            }
+        }
+
+        public List<Data.Orm.Blog> GetByMenuSlug(string pageSlug)
+        {
+            using(ServicePoint services = new ServicePoint())
+            {
+                throw new NotFiniteNumberException();
+            }
+        }
+    }
+}
---
+++ BlogExample.WCF/BlogServices/IBlogOperation.cs
@@ -0,0 +1,26 @@
+﻿using BlogExample.Data.Orm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogExample.WCF.BlogServices
+{
+    [ServiceContract]
+    public interface IBlogOperation
+    {
+        [OperationContract]
+        List<Blog> GetAll();
+
+        /// <summary>
+        /// Girilen menünün slug'ına göre blogları getirir.
+        /// </summary>
+        /// <param name="pageSlug"></param>
+        /// <returns></returns>
+        
+        [OperationContract]
+        List<Blog> GetByMenuSlug(string pageSlug);
+    }
+}
